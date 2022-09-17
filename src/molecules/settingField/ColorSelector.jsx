741c68d5c89a1a7602909f6084534dd80f6bdc10
backtofayton <yucelolcay@gmail.com>
2022-09-17 16:39:29 +0300

    Add state to ColorSelector

--- src/molecules/settingField/ColorSelector.jsx
+++ src/molecules/settingField/ColorSelector.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 import classes from "./.module.scss";
 
-export function ColorSelector({ title }) {
+export function ColorSelector({ title, defaultValue = "#000000" }) {
+  const [value, setValue] = useState(defaultValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className={classes.singleAttribute}>
       <p>{title}</p>
       <div className={classes.colorInputContainer}>
         <label htmlFor="input-color">
-          <span className={classes.colorPicker}></span>
+          <span
+            className={classes.colorPicker}
+            style={{ backgroundColor: value }}
+          ></span>
         </label>
-        <input className={classes.input}></input>
-        <input id="input-color" type="color" className={classes.inputColor} />
+        <input
+          className={classes.input}
+          value={value}
+          onChange={handleChange}
+        />
+        <input
+          id="input-color"
+          type="color"
+          value={value}
+          onChange={handleChange}
+          className={classes.inputColor}
+        />
       </div>
     </div>
   );
