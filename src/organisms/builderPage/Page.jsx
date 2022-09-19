400a14669d12f2bc747b113f3dc86088f9dc5bd0
backtofayton <yucelolcay@gmail.com>
2022-09-19 15:39:54 +0300

    add mockdata

--- src/organisms/builderPage/Page.jsx
+++ src/organisms/builderPage/Page.jsx
@@ -171,36 +171,3 @@ export function Page() {
     </>
   );
 }
-
-const mockData = {
-  incentive1: {
-    title: {
-      fontFamily:
-        '"SF Pro Display", -apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-      lineHeight: "20px",
-      fontSize: "24px",
-      fontColor: "white",
-      padding: "0 0 18px 0",
-      margin: 0,
-      visibility: true,
-    },
-    subTitle: {
-      fontFamily:
-        '"SF Pro Display", -apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-      lineHeight: "20px",
-      fontSize: "18px",
-      fontColor: "white",
-      padding: "0 0 18px 0",
-      margin: 0,
-      visibility: true,
-    },
-    countdown: {
-      duration: "5",
-      visbility: true,
-    },
-    background: { color: "rgb(0, 128, 96)" },
-    layout: {
-      padding: "22px 10px 2px 10px",
-    },
-  },
-};
