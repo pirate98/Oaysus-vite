fac6f9fc771a9818d6d3f0d301ca3328455ce087
backtofayton <yucelolcay@gmail.com>
2022-09-19 15:42:13 +0300

    update

--- src/data/mockPages.js
+++ src/data/mockPages.js
@@ -39,7 +39,7 @@ export default [{
   //   }
   // },
   content1: {
-    image: ["http://127.0.0.1:5173/image/guy_1.jpg"],
+    image: [{url:"http://127.0.0.1:5173/image/guy_1.jpg"}],
     title: {
       text: "Test T-shirt",
       fontFamily:
@@ -78,6 +78,7 @@ export default [{
       visibility: true,
     },
     subTitle: {
+      text:"You could highlight specific ingredients, materials, or functionality that make your product unique, and explain how it will improve the customer's life."
       fontFamily:
         '"SF Pro Display", -apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       lineHeight: "20px",
