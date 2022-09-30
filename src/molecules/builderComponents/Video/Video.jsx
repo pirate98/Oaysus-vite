9de0d3f95dbc867f0ec77ff6f34fbacd49b49cd4
backtofayton <yucelolcay@gmail.com>
2022-09-30 18:24:20 +0300

    positioning fix

--- src/molecules/builderComponents/Video/Video.jsx
+++ src/molecules/builderComponents/Video/Video.jsx
@@ -11,37 +11,33 @@ const fn = forwardRef(({ content }, ref) => {
   const styles = filterOnlyStyleValues(content);
 
   return (
-    <Grid
-      // container
-      item
-      xs={12}
-      ref={ref}
-      className={classes.componentContainer}
-    >
-      <EditableWithToolBar
-        name="title"
-        module="title"
-        data-oa-type="text"
-        type="h3"
-        style={{
-          ...styles.title,
-          display: content.title.visibility ? "inherit" : "none",
-        }}
-        className={classes.title}
-      >
-        {content?.title?.editorState}
-      </EditableWithToolBar>
-      {/* <p className={classes.videoTitle}></p> */}
-      <div
-        className={classes.imageZone}
-        style={{
-          backgroundImage: false
-            ? false
-            : 'url("/image/play.svg"), url("/image/empty-video.svg")',
-          backgroundSize: false ? "cover" : "unset",
-        }}
-      ></div>
-    </Grid>
+    <div className={classes.componentContainer} ref={ref}>
+      <section>
+        <EditableWithToolBar
+          name="title"
+          module="title"
+          data-oa-type="text"
+          type="h3"
+          style={{
+            ...styles.title,
+            display: content.title.visibility ? "inherit" : "none",
+          }}
+          className={classes.title}
+        >
+          {content?.title?.editorState}
+        </EditableWithToolBar>
+        {/* <p className={classes.videoTitle}></p> */}
+        <div
+          className={classes.imageZone}
+          style={{
+            backgroundImage: false
+              ? false
+              : 'url("/image/play.svg"), url("/image/empty-video.svg")',
+            backgroundSize: false ? "cover" : "unset",
+          }}
+        ></div>
+      </section>
+    </div>
   );
 });
 
