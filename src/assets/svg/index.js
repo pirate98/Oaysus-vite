7649ddeeb92110f731d7c412396480c58b883adc
Jason Biondo <jason.m.biondo@gmail.com>
2022-10-21 10:12:36 -0400

    new logo

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,6 +1,6 @@
 export { default as notFoundImage } from "./emptyState.svg";
 export { default as trophyImage } from "./homeTrophy.png";
-export { default as logo } from "./logo.png";
+export { default as logo } from "./logo2.png";
 export { default as upsells } from "./upsells.svg";
 export { default as performance } from "./performance.svg";
 // export { default as emptyImage } from "./empty-image.svg";
